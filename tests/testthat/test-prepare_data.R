ca12e6c3d0d2cfe5062a8595c914bfc42fec5fcc
majazaloznik <maja.zaloznik@gmail.com>
2025-10-07 11:16:12 +0200

    Fix tests to work without file access on gha

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -1,19 +1,43 @@
-test_that("mock tests table prep without db access", {
+test_that("mock tests table prep without db or file access", {
   dittodb::with_mock_db({
     con_test <- make_test_connection()
-    file_paths_df <- get_all_recent_files()
+
+    # Mock file_paths_df instead of calling get_all_recent_files()
+    file_paths_df <- data.frame(
+      source_name = c("bruto_placa", "delovno_aktivni", "delovno_aktivni_brez_kmetov",
+                      "delovno_aktivni_disagr", "ilo_brezposelni", "ilo_stopnja",
+                      "ilo_zaposleni", "reg_brezposelni", "reg_brezposelni_stopnja"),
+      file_path = rep(testthat::test_path("testdata", "test_single.xlsx"), 9),
+      file_mtime = rep(Sys.time(), 9),
+      stringsAsFactors = FALSE
+    )
+
     x <- prepare_vintage_table(file_paths_df, con_test, schema = "platform")
-    expect_equal(dim(x), c(34,2))
+    expect_equal(dim(x), c(34, 2))
     expect_equal(names(x), c("series_id", "published"))
-    file_paths_df <- get_all_recent_files()
-    data <- extract_all_desezoniranje_data(file_paths_df)
-    datapoint_tables <- purrr::imap(data, ~{
+
+    # Mock extracted data instead of calling extract_all_desezoniranje_data()
+    mock_data <- list(
+      BP_Orig = data.frame(
+        period_id = c("2024M01", "2024M02", "2024M03"),
+        VSI = c(100, 110, 120),
+        BK = c(200, 210, 220)
+      ),
+      BP_SA = data.frame(
+        period_id = c("2024M01", "2024M02", "2024M03"),
+        VSI = c(105, 115, 125),
+        BK = c(205, 215, 225)
+      )
+    )
+
+    datapoint_tables <- purrr::imap(mock_data, ~{
       prepare_datapoint_table(.x, .y, con_test)
     })
-    expect_equal(length(datapoint_tables), 8)
+
+    expect_equal(length(datapoint_tables), 2)
     expect_equal(ncol(datapoint_tables[[1]]$data), 5)
     expect_equal(names(datapoint_tables[[1]]$data), c("time", "Meritev",
-                                                 "Seasonally.adjusted",
-                                                 "value", "flag"))
+                                                      "Seasonally.adjusted",
+                                                      "value", "flag"))
   })
 })
